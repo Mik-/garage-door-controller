b66be6d49f315d93c56e7439d2190dac0265e067
Michael Neuendorf <michael@neuendorf-online.de>
2015-08-13 20:28:35 +0200

    Initial commit of project skeleton

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Garage door opener',
+    'author': 'Michael Neuendorf',
+    'url': 'www.neuendorf-online.de',
+    'download_url': 'www.neuendorf-online.de',
+    'author_email': 'michael@neuendorf-online.de',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['garage'],
+    'scripts': [],
+    'name': 'garage'
+}
+
+setup(**config)
