d7a43ff8abe497af1d8fa8b925a5832549f51d15
Michael Neuendorf <michael@neuendorf-online.de>
2015-08-17 21:33:31 +0200

    Added signals to driver test

--- test_rpi_driver.py
+++ test_rpi_driver.py
@@ -1,14 +1,23 @@
 from garage.door.rpi_driver import RPiDriver
 import RPi.GPIO as GPIO
 import time
+from blinker import signal
+from garage.door.signals import *
+
 
 driver = RPiDriver(18, 17, 27)
+
+def switch_status(sender):
+    print "Door position: " + driver.get_door_position()
 
 print "RPi-Driver test"
 raw_input("Press [Enter] to start test")
 
 print "The relay is off, the switches are not closed"
 raw_input("Press [Enter] to continue")
+
+signal(SIGNAL_LOWER_SWITCH_CHANGED).connect(switch_status)
+signal(SIGNAL_UPPER_SWITCH_CHANGED).connect(switch_status)
 
 print "Close lower limit switch"
 while driver.get_lower_limit_switch_state() == False:
@@ -17,6 +26,9 @@ while driver.get_lower_limit_switch_state() == False:
 print "Close upper limit switch"
 while driver.get_upper_limit_switch_state() == False:
     pass
+
+signal(SIGNAL_LOWER_SWITCH_CHANGED).disconnect(self._switch_changed)
+signal(SIGNAL_UPPER_SWITCH_CHANGED).disconnect(self._switch_changed)
 
 print "Relais on"
 driver.start_door_signal()
