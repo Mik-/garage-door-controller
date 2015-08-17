e95421c37bb4c3cd28e5d92df9bd25b818ee0ba6
Michael Neuendorf <michael@neuendorf-online.de>
2015-08-17 21:41:25 +0200

    Bugfix: driver test

--- test_rpi_driver.py
+++ test_rpi_driver.py
@@ -4,11 +4,17 @@ import time
 from blinker import signal
 from garage.door.signals import *
 
-
+# Setup the driver for the specified gpios
+# 18 relay
+# 17 lower limit switch
+# 27 upper limit switch
 driver = RPiDriver(18, 17, 27)
 
 def switch_status(sender):
-    print "Door position: " + driver.get_door_position()
+    """This is a callable, which is called on edge detection by the driver."""
+    print "Upper limit switch: %r" % driver.get_upper_limit_switch_state()
+    print "Lower limit switch: %r" % driver.get_lower_limit_switch_state()
+
 
 print "RPi-Driver test"
 raw_input("Press [Enter] to start test")
@@ -27,8 +33,8 @@ print "Close upper limit switch"
 while driver.get_upper_limit_switch_state() == False:
     pass
 
-signal(SIGNAL_LOWER_SWITCH_CHANGED).disconnect(self._switch_changed)
-signal(SIGNAL_UPPER_SWITCH_CHANGED).disconnect(self._switch_changed)
+signal(SIGNAL_LOWER_SWITCH_CHANGED).disconnect(switch_status)
+signal(SIGNAL_UPPER_SWITCH_CHANGED).disconnect(switch_status)
 
 print "Relais on"
 driver.start_door_signal()
